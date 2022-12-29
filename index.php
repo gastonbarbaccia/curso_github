f4f944729666f55905bb1dae12350db66dc12128
gastonbarbaccia <gastonbarbaccia@hotmail.com>
2022-12-29 20:37:05 -0300

    ok

--- index.php
+++ index.php
@@ -4,9 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Github + Git</title>
+    <title>Github + Git + devops + aws</title>
 </head>
 <body>
-    
+
+    <?php echo "Hola desde AWS"?>
+
 </body>
 </html>
